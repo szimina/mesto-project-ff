5b08ee5fe8a8385b9e14a118e63697f9de3f132d
Svetlana Zimina <s.tsapenko@yandex.ru>
2023-11-02 15:31:25 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const content = document.querySelector(".content");
+const placesList = content.querySelector(".places__list");
 
-// @todo: DOM узлы
+function addCard(cardItem) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
-// @todo: Функция создания карточки
+  cardElement.querySelector(".card__title").textContent = cardItem.name;
+  cardElement.querySelector(".card__image").src = cardItem.link;
+  cardElement.querySelector(".card__image").alt = cardItem.name;
 
-// @todo: Функция удаления карточки
+  placesList.append(cardElement);
 
-// @todo: Вывести карточки на страницу
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", function () {
+      deleteCardElement(cardElement);
+  });
+}
+
+for (let i = 0; i < initialCards.length; i = i + 1) {
+  cardItem = initialCards[i];
+  addCard(cardItem);
+}
+
+function deleteCardElement(elem) {
+  elem.remove();
+}
